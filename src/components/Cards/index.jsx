14dd277333c45d464b33773531fe3a2a170ae4a5
kevilin.rodrigues <kevilin.rodrigues@kinghost.com.br>
2023-04-21 21:26:47 -0300

    Adicionando feature

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,20 +1,43 @@
 import React from "react";
-import { Box, Divider, HStack, Text, Icon, Image, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  HStack,
+  Text,
+  Icon,
+  Image,
+  Button,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { BiGitRepoForked, BiCodeAlt } from "react-icons/bi";
 import { VscGithub } from "react-icons/vsc";
 
-
 const MotionBox = motion(Box);
 
 const Cards = ({ post }) => {
-  const { projeto, url_site, url_github, tecnologias, url_imagem } = post.data().inputs;
+  const { projeto, url_site, url_github, tecnologias, url_imagem } =
+    post.data().inputs;
 
   return (
     <MotionBox initial={{ opacity: 0 }} animate={{ opacity: 1, y: -10 }}>
-      <Box maxW="sm" borderRadius="xl" w={{ base: "100%", md: "23rem" }} p={4} bg="brand.bg" overflow="hidden" minH="25rem" minW="42rem" boxShadow="xl">
-
-        <Box maxW="xl" minW="40rem" overflow="hidden" rounded="20px" maxHeight="20rem">
+      <Box
+        maxW="sm"
+        borderRadius="xl"
+        w={{ base: "100%", md: "100%" }}
+        p={4}
+        bg="brand.bg"
+        overflow="hidden"
+        minH="25rem"
+        minW="42rem"
+        boxShadow="xl"
+      >
+        <Box
+          maxW="xl"
+          minW="40rem"
+          overflow="hidden"
+          rounded="20px"
+          maxHeight="20rem"
+        >
           <Image
             src={url_imagem.length === 0 ? null : url_imagem[0].url_imagem}
             alt={url_imagem.length === 0 ? null : url_imagem[0].imageData}
@@ -33,25 +56,34 @@ const Cards = ({ post }) => {
         </Text>
         <HStack color="gray" fontSize="sm">
           <span role="img">
-            <VscGithub fontSize="1.2rem"/>
+            <VscGithub fontSize="1.2rem" />
           </span>
-          <a href={`${url_github}`} target="_blank">Repositório</a>
+          <a href={`${url_github}`} target="_blank">
+            Repositório
+          </a>
         </HStack>
         <Divider mt={2} borderColor="gray" m=".5rem 0" />
         <HStack as="span" fontSize="sm">
           {tecnologias === "" ? null : (
             <HStack>
-              <Icon as={BiCodeAlt} color="gray" fontSize="1.2rem" /> <Text>Tecnologias: {tecnologias}</Text>
+              <Icon as={BiCodeAlt} color="gray" fontSize="1.2rem" />{" "}
+              <Text>Tecnologias: {tecnologias}</Text>
             </HStack>
           )}
         </HStack>
-
-          <a href={`${url_site}`} target="_blank">
-          <Button role="button" w="100%" mt={4} bg="brand.btn" onClick={() => window.scrollTo({ top: 0 })} color="brand.bg" _hover={{ bg: "#789b8b" }}>
+        <a href={`${url_site}`} target="_blank">
+          <Button
+            role="button"
+            w="100%"
+            mt={4}
+            bg="brand.btn"
+            onClick={() => window.scrollTo({ top: 0 })}
+            color="brand.bg"
+            _hover={{ bg: "#789b8b" }}
+          >
             Acessar
           </Button>
-          </a>
-
+        </a>
       </Box>
     </MotionBox>
   );
